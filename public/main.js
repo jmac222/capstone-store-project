10f356e96a56a5a2a28ff97d439c86a40b55ef83
Daniel Chavez Ceniceros <Dchave263@west-mec.local>
2021-12-14 14:28:06 -0700

    finished homepage basics

--- public/main.js
+++ public/main.js
@@ -1,23 +1,33 @@
-
-
+ 
+ 
 const url = "/api/v1";
 const container = document.querySelector(".container");
-
+const quant = document.querySelector("#quant")
+ 
+ 
+async function quantityAdd() {
+ 
+}
+ 
+ 
 async function fetchProducts() {
   try {
     const {
       data: { products }
-      
+     
     } = await axios.get(`${url}/upload`);
-    
+   
     const tempProducts = products.map(each => {
+     
       return `<article class="product">
       <img src="${each.image}" alt="${each.name}" class="img" />
       <footer>
       <p>${each.name}</p>
-      <span>${each.price}</span>
+      <span>$${each.price}</span>
+      <button id = "quant" onclick = "logging(4)">+${each.quantity}</button>
       </footer>
       </article>`;
+     
     })
     .join("");
     container.innerHTML = tempProducts;
@@ -25,5 +35,5 @@ async function fetchProducts() {
    console.log(error);
   }
 }
-
+ 
 fetchProducts();
